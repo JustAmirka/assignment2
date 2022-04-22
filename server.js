0d1db5add5bfd999e0dc0411dfb045da047f9706
JustAmirka <97117667+JustAmirka@users.noreply.github.com>
2022-04-22 18:19:18 +0600

    api

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyParser = require('body-parser')
 const app = express();
 const ejs=require('ejs');
-const port = 3000;
+const port = 4000;
 
 
 app.set('view engine', 'ejs');
